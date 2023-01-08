4673301612101c3117f883f58dead726f043a475
pedromlsr <pedrodesarodrigues@gmail.com>
2023-01-08 12:42:19 -0300

    person service (only create method) + mappers

---
+++ MP.ApiDotNet6/MP.ApiDotNet6.Application/Services/Interfaces/IPersonService.cs
@@ -0,0 +1,9 @@
+﻿using MP.ApiDotNet6.Application.DTOs;
+
+namespace MP.ApiDotNet6.Application.Services.Interfaces
+{
+    public interface IPersonService
+    {
+        Task<ResultService<PersonDTO>> CreateAsync(PersonDTO personDTO);
+    }
+}
