2855c86af634249c17ad4c7514d61e6ad16d5984
pedromlsr <pedrodesarodrigues@gmail.com>
2023-01-07 22:57:04 -0300

    Person entity + generic validation

---
+++ MP.ApiDotNet6/MP.ApiDotNet6.Domain/Entities/Person.cs
@@ -0,0 +1,37 @@
+﻿using MP.ApiDotNet6.Domain.Validations;
+
+namespace MP.ApiDotNet6.Domain.Entities
+{
+    public sealed class Person
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Document { get; private set; }
+        public string Phone { get; private set; }
+
+        public Person(string name, string document, string phone)
+        {
+            Validation(name, document, phone);
+        }
+
+        public Person(int id, string name, string document, string phone)
+        {
+            DomainValidationException.When(id < 0, "Id deve ser maior que zero!");
+
+            Id = id;
+
+            Validation(name, document, phone);
+        }
+
+        private void Validation(string name, string document, string phone)
+        {
+            DomainValidationException.When(string.IsNullOrEmpty(name), "Nome deve ser informado!");
+            DomainValidationException.When(string.IsNullOrEmpty(document), "Documento deve ser informado!");
+            DomainValidationException.When(string.IsNullOrEmpty(phone), "Telefone deve ser informado!");
+
+            Name = name;
+            Document = document;
+            Phone = phone;
+        }
+    }
+}
---
+++ MP.ApiDotNet6/MP.ApiDotNet6.Domain/Validations/DomainValidationException.cs
@@ -0,0 +1,12 @@
+﻿namespace MP.ApiDotNet6.Domain.Validations
+{
+    internal class DomainValidationException : Exception
+    {
+        public DomainValidationException(string error) : base(error) { }
+    
+        public static void When(bool hasError, string message)
+        {
+            if (hasError) throw new DomainValidationException(message);
+        }
+    }
+}
