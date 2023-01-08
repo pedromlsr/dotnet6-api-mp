4c81848575a50f3070385565465600a13baef032
pedromlsr <pedrodesarodrigues@gmail.com>
2023-01-08 11:28:07 -0300

    person and product repositories

---
+++ MP.ApiDotNet6/MP.ApiDotNet6.Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using MP.ApiDotNet6.Domain.Entities;
+
+namespace MP.ApiDotNet6.Domain.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<ICollection<Product>> GetAllAsync();
+        Task<Product> GetByIdAsync(int id);
+        Task<Product> CreateAsync(Product Product);
+        Task EditAsync(Product Product);
+        Task DeleteAsync(Product Product);
+    }
+}
--- MP.ApiDotNet6/MP.ApiDotNet6.Infra.Data/Context/ApplicationDbContext.cs
+++ MP.ApiDotNet6/MP.ApiDotNet6.Infra.Data/Context/ApplicationDbContext.cs
@@ -8,6 +8,8 @@ namespace MP.ApiDotNet6.Infra.Data.Context
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public DbSet<Person> People { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
